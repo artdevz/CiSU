daea9eac172bc716c21f03ae1a428ab921e10234
artdevz <prodevarthurdantas@gmail.com>
2024-09-10 23:37:43 -0300

    First Commit

---
+++ src/test/java/com/CaniNUS/CaniNusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CaniNUS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaniNusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
